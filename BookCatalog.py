4526200dcd021b2903bb0391a65f06066567dbbc
Destriana Ramadani <destrianaramadani2@gmail.com>
2024-01-22 01:47:07 +0700

    add ngc 4

--- BookCatalog.py
+++ BookCatalog.py
@@ -11,36 +11,36 @@ class BookCatalog:
     def addBook(self):
         for book in BookCatalog.books: 
             if book[2] == self.identifier: 
-                raise ValueError(f"Book with ID {self.identifier} already exists")
+                return "This book already exists"
 
-        BookCatalog.books.append((self.title, self.author, self.identifier)) 
-
+        self.books.append([self.title, self.author, self.identifier]) 
+    
     # Function to search a book in catalog   
     def searchBook(self, keysearch):
         if len(BookCatalog.books) == 0:
-            raise ValueError("Book catalog is empty")
+            return "Book catalog is empty"
         
         for book in BookCatalog.books:
             for bookIdentity in book:
                 if bookIdentity.lower() == keysearch.lower():
                     return book
                 
-        raise Exception("This book is not found in the catalog")
+        return "This book is not found in the catalog"
     
     # Function to remove a book from the catalog          
     def removeBook(self, title):
         if len(BookCatalog.books) == 0:
-            raise ValueError("Book catalog is empty")
+            return "Book catalog is empty"
         
         for book in BookCatalog.books:
             for bookIdentity in book:
                 if bookIdentity.lower() == title.lower():
-                    BookCatalog.books.remove(book)
-                    
-        raise Exception("This book is not found in the catalog") 
+                    self.books.remove(book)
+    
+        return "This book is not found in the catalog"
                    
     # Function to show a book in catalog
     def showCatalog(self):
         if len(BookCatalog.books) == 0:
-            raise ValueError("Book catalog is empty")
+            return "Book catalog is empty"
         return BookCatalog.books
